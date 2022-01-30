3bb59db6d92f10eba44226c12d8c71fead6dbf34
Milena Morais <milenaferreira96@hotmail.com>
2022-01-29 23:25:14 -0300

    Criação do componente toggle

---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+    Container,
+    ToggleLabel 
+} from './styles';
+
+const Toggle: React.FC = () => (
+    <Container>
+        <ToggleLabel>Light</ToggleLabel>
+        <ToggleLabel>Dark</ToggleLabel>
+    </Container>
+
+    
+);
+
+export default Toggle; 
