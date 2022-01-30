16e31f67f4af934f4362fdd11b8b6eff3976c0ca
Milena Morais <milenaferreira96@hotmail.com>
2022-01-29 23:38:29 -0300

    Criando o switch no toggle

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Switch from 'react-switch';
 
 import {
     Container,
@@ -8,10 +9,12 @@ import {
 const Toggle: React.FC = () => (
     <Container>
         <ToggleLabel>Light</ToggleLabel>
+        <Switch
+            checked
+            onChange={() => console.log('mudou')}
+        />
         <ToggleLabel>Dark</ToggleLabel>
     </Container>
-
-    
-);
+)
 
 export default Toggle; 
